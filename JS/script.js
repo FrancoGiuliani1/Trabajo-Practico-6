5ae8c047a6f1cbb27ad0a10b68be2ab5eb606966
Franco <you@example.com>
2022-10-07 23:36:56 -0300

    validacion del calculo

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,13 @@
 const nombre = document.getElementById('nombre');
 const apellido = document.getElementById('apellido');
 const calculo = document.getElementById('calculo');
+
+form.addEventlistener('submit', e=>{
+    e.preventDefault
+      if (calculo.value =25){
+  
+      } else{
+          alert('Calculo incorrecto');
+      }
+  
+  })
